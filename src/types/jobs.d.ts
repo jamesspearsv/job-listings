73b9aed70ef6e7eb70f9296ac17fe4e4406f7e1e
James Spears <jamesspearsv@gmail.com>
2025-01-12 10:47:08 -0500

    Add typing for job listings

---
+++ src/types/jobs.d.ts
@@ -0,0 +1,17 @@
+export type JobListing = {
+  id: number;
+  company: string;
+  logo: string; // image url string
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+};
+
+export type Response = JobListing[];
